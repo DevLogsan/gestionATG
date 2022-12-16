35e9fb725c45211873e792d355c1792cf82559bc
DevLogsan <loganlegallou22@gmail.com>
2022-12-16 12:04:25 +0100

    fxml inserrer bureau

--- src/controleur/FenFXML_bureauController.java
+++ src/controleur/FenFXML_bureauController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controleur;
 
 import java.io.IOException;
@@ -22,16 +17,9 @@ import javafx.stage.Stage;
 import modele.GestionBureau;
 import modele.Bureau;
 
-/**
- * FXML Controller class
- *
- * @author Logan
- */
 public class FenFXML_bureauController implements Initializable {
     @FXML
-    private Label lblMessage;
-    @FXML
-    private TableView<Bureau> tvMembres; // Même nom que le TableView de l'interface graphique
+    private TableView<Bureau> tvBureaux; // Même nom que le TableView de l'interface graphique
     @FXML
     private TableColumn<Bureau, Integer> colonneId; // Même nom que la colonne du TableView
     @FXML
@@ -49,8 +37,7 @@ public class FenFXML_bureauController implements Initializable {
     @FXML
     private TableColumn<Bureau, String> colonneEmail; // Même nom que la colonne du TableView
     @FXML
-    private TableColumn<Bureau, String> colonnePortable; // Même nom que la colonne du TableView
-    
+    private TableColumn<Bureau, String> colonnePortable; // Même nom que la colonne du TableView 
     @FXML
     private Stage premierStage;
     
@@ -60,7 +47,7 @@ public class FenFXML_bureauController implements Initializable {
         ObservableList<Bureau> lesBureaux = FXCollections.observableArrayList();
         
         lesBureaux = GestionBureau.listeBureaux();
-        tvMembres.setItems(lesBureaux);
+        tvBureaux.setItems(lesBureaux);
         
         // Initialise le TableView tvEtudiants
         colonneId.setCellValueFactory(new PropertyValueFactory<Bureau, Integer>("id"));
@@ -74,11 +61,11 @@ public class FenFXML_bureauController implements Initializable {
         colonnePortable.setCellValueFactory(new PropertyValueFactory<Bureau, String>("telPortable"));
         
         // Pour redimensionner les colonnes automatiquement
-        tvMembres.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tvBureaux.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     }
     
     @FXML
-    private void handleInsererBureau()
+    public void handleEnregistrerUnBureau() // handle du bouton 'ajouter' dans la liste des membres du bureau
     {
         try
         {
@@ -95,5 +82,65 @@ public class FenFXML_bureauController implements Initializable {
         {
         System.out.println("Erreur chargement seconde fenetre : " + e.getMessage());
         }
+    }
+    
+    @FXML
+    public void handleSupprimerBureau()
+    {
+        Bureau leBureau = tvBureaux.getSelectionModel().getSelectedItem();
+        GestionBureau.supprimerBureau(leBureau);
+        
+        ObservableList<Bureau> lesBureaux = FXCollections.observableArrayList();
+        
+        lesBureaux = GestionBureau.listeBureaux();
+        tvBureaux.setItems(lesBureaux);
+        
+        // Initialise le TableView
+        colonneId.setCellValueFactory(new PropertyValueFactory<Bureau, Integer>("id"));
+        colonneFonction.setCellValueFactory(new PropertyValueFactory<Bureau, String>("fonction"));
+        colonneNom.setCellValueFactory(new PropertyValueFactory<Bureau, String>("nom"));
+        colonnePrenom.setCellValueFactory(new PropertyValueFactory<Bureau, String>("prenom"));
+        colonneAdresse.setCellValueFactory(new PropertyValueFactory<Bureau, String>("adresse"));
+        colonneCP.setCellValueFactory(new PropertyValueFactory<Bureau, String>("cp"));
+        colonneVille.setCellValueFactory(new PropertyValueFactory<Bureau, String>("ville"));
+        colonneEmail.setCellValueFactory(new PropertyValueFactory<Bureau, String>("email"));
+        colonnePortable.setCellValueFactory(new PropertyValueFactory<Bureau, String>("telPortable"));
+        
+        // Pour redimensionner les colonnes automatiquement
+        tvBureaux.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+    
+    @FXML
+    public void handleModifierBureau()
+    {
+        Bureau leBureau = tvBureaux.getSelectionModel().getSelectedItem();
+        GestionBureau.modifierBureau(leBureau);
+        MainApp.setUnBureauSelectionner(leBureau);
+        
+        try
+        {
+            premierStage = new Stage();
+            premierStage.setTitle("Modification des infos");
+            FXMLLoader loader = new FXMLLoader(FXMLMenuController.class.getResource("/vue/fenFXML_ModifBureau.fxml"));
+            
+            AnchorPane rootLayout = (AnchorPane) loader.load(); // probleme ici
+            
+            Scene scene = new Scene(rootLayout);
+            premierStage.setScene(scene);
+            premierStage.show();
+            
+            
+            
+        }
+        catch (Exception e)
+        {
+        System.out.println("Erreur chargement seconde fenetre : " + e.getMessage());
+        }
+    }
+    
+    @FXML
+    public void handleConfirmerModifierBureau()
+    {
+        System.out.print("Oui");
     }
 }
